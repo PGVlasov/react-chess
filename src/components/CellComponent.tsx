0d1b2b9a376117bbbd0e3b3c0ce3650cdd066d74
PGVlasov <PGVlasov@users.noreply.github.com>
2022-05-17 00:58:04 +0300

    board is ready

---
+++ src/components/CellComponent.tsx
@@ -0,0 +1,9 @@
+import { Cell } from "../models/Cell";
+
+interface cellProps {
+  cell: Cell;
+}
+
+export const CellComponent: React.FC<cellProps> = ({ cell }) => {
+  return <div className={["cell", cell.color].join(" ")}></div>;
+};
